99a085663ae46f7c7bb9c5b0e6fa4037a4dc63a0
sarthak78381 <sarthakg2k29@gmail.com>
2021-02-22 17:56:41 +0530

    added responsive header

---
+++ src/components/header/header-pc/header-pc.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './header-pc.scss';
+import HeaderUserInfo from '../../headerUserInfo/headerUserInfo';
+
+
+const HeaderPc = () => {
+    return (
+    <div className="header__container">
+        <div className="header">
+            <div>
+                <div className="header__logoContainer">
+                    <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className='header__logo'/>
+                </div>
+            </div>
+            <div className='searchBox__Container'>
+                <div className="header__searchContainer">
+                    <i className="fas fa-search header__searchIcon"></i>
+                    <input type="text" placeholder='search' className='header__search'/>
+                </div>
+            </div>
+            <HeaderUserInfo />
+        </div>
+    </div>
+)}
+
+
+export default HeaderPc;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import HeaderUserInfo from '../headerUserInfo/headerUserInfo'
-import './header.scss';
+import React, {useState} from 'react';
+import HeaderPc from './header-pc/header-pc'
+import HeaderMobile from './header-mobile/header-mobile'
 
-const Header = () => (
-    <div className="header__container">
-        <div className="header">
-            <div>
-                <div className="header__logoContainer">
-                    <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className='header__logo'/>
-                </div>
-            </div>
-            <div>
-                <div className="header__searchContainer">
-                    <i className="fas fa-search header__searchIcon"></i>
-                    <input type="text" placeholder='search' className='header__search'/>
-                </div>
-            </div>
-            <HeaderUserInfo />
-        </div>
-    </div>
-)
+
+const Header = () => {
+
+
+    const [changedValue, onChange] = useState(window.innerWidth);
+
+    window.addEventListener('resize', () => {
+        onChange(window.innerWidth);
+    })
+    return changedValue < 600 ? (<HeaderMobile />):(<HeaderPc />);
+}
 
 export default Header;
